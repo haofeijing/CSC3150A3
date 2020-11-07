88e1a5a25ed401cc4951055f237ef37c11f208bd
hanyinbin <hyb117010083@outlook.com>
2020-11-07 23:19:36 +0800

    Add read

--- CSC3150A3/virtual_memory.cu
+++ CSC3150A3/virtual_memory.cu
@@ -49,25 +49,50 @@ __device__ uchar vm_read(VirtualMemory *vm, u32 addr) {
 	int hit_slot = -1;
 	for (int i = vm->PAGE_ENTRIES-1; i > -1; i--) {
 		if (vm->invert_page_table[i] != 0x80000000) {
+			if (vm->invert_page_table[i] % (1 << 12) == page_num) {
+				hit = true;
+				hit_slot = i;
+			}
 			vm->invert_page_table[i] += (1 << 13);
 		}
 
-		if (vm->invert_page_table[i] % (1 << 12) == page_num) {
-			hit = true;
-			hit_slot = i;
-		}
+
 	}
 	int frame_num;
-	// if (hit) {
-	// 	frame_num = vm->invert_page_table[hit_slot + vm->PAGE_ENTRIES];
-	// 	u32 phy_addr = frame_num * vm->PAGESIZE + page_offset;
-	// 	return vm->buffer[phy_addr];
-	// } else {
+	if (hit) {
+		frame_num = vm->invert_page_table[hit_slot + vm->PAGE_ENTRIES];
+		u32 phy_addr = frame_num * vm->PAGESIZE + page_offset;
+		return vm->buffer[phy_addr];
+	} else {
+		*vm->pagefault_num_ptr += 1; // add page fault;
+		// find lru
+		int max_time = -1;
+		int least_used_slot;
+		for (int i = vm->PAGE_ENTRIES-1; i > -1; i--) {
+			if (vm->invert_page_table[i] != 0x80000000) {
+				int tmp_time = vm->invert_page_table[i] / (1 << 13);
+				if (tmp_time > max_time) {
+					max_time = tmp_time;
+					least_used_slot = i;
+				}
+			}
+		}
 		
-	// }
 
+		frame_num = vm->invert_page_table[least_used_slot + vm->PAGE_ENTRIES];
+		int old = vm->invert_page_table[least_used_slot] % (1 << 12);
+		for (int i = 0; i < vm->PAGESIZE; i++) {
+			// swap out
+			vm->storage[old * vm->PAGESIZE + i] = vm->buffer[frame_num * vm->PAGESIZE + i];
+			// swap in
+			vm->buffer[frame_num * vm->PAGESIZE + i] = vm->storage[page_num * vm->PAGESIZE + i]
+		}
+		u32 phy_addr = frame_num * vm->PAGESIZE + page_offset;
+		vm->invert_page_table[least_used_slot] = page_num;
+		return vm->buffer[phy_addr];
+	
+	}
 
-	return;
 }
 
 __device__ void vm_write(VirtualMemory *vm, u32 addr, uchar value) {
@@ -125,7 +150,7 @@ __device__ void vm_write(VirtualMemory *vm, u32 addr, uchar value) {
 			}
 			u32 phy_addr = frame_num * vm->PAGESIZE + page_offset;
 			vm->buffer[phy_addr] = value;
-
+			vm->invert_page_table[least_used_slot] = page_num;
 		} else {
 			// empty exists, write in value in page_number
 			vm->invert_page_table[empty_slot] = page_num;
